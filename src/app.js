756f018d5584be6e088ea2d8e44ebdee2b3d6580
Rayan CAPALDI <rayan.capaldi@imogate.eu>
2024-06-07 10:41:19 +0200

    no .env file

--- src/app.js
+++ src/app.js
@@ -1,20 +1,19 @@
-require('dotenv').config({ path: './var.env' })
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-const tickets_routes = require('./routes/routesTicket.js')
-const clients_routes = require('./routes/routesClient.js')
-const developers_routes = require('./routes/routesDeveloper.js')
-const projects_routes = require('./routes/routesProject.js')
-const comments_routes = require('./routes/routesComment.js')
-const accesses_routes = require('./routes/routesAccess.js')
-const tags_routes = require('./routes/routesTag.js')
-const ticketTags_routes = require('./routes/routesTicketTag.js')
+const tickets_routes = require('./routes/routesTicket.js');
+const clients_routes = require('./routes/routesClient.js');
+const developers_routes = require('./routes/routesDeveloper.js');
+const projects_routes = require('./routes/routesProject.js');
+const comments_routes = require('./routes/routesComment.js');
+const accesses_routes = require('./routes/routesAccess.js');
+const tags_routes = require('./routes/routesTag.js');
+const ticketTags_routes = require('./routes/routesTicketTag.js');
 
 const port = process.env.PORT || 3000;
 const uri = process.env.MONGODB_URI;
@@ -23,15 +22,15 @@ mongoose.connect(uri)
   .then(() => console.log('Connected to MongoDB'))
   .catch(error => console.error('Error:', error));
 
-app.use(express.json())
-app.use('/api/tickets', tickets_routes)
-app.use('/api/clients', clients_routes)
-app.use('/api/developers', developers_routes)
-app.use('/api/projects', projects_routes)
-app.use('/api/comments', comments_routes)
-app.use('/api/accesses', accesses_routes)
-app.use('/api/tags', tags_routes)
-app.use('/api/ticketTags', ticketTags_routes)
+app.use(express.json());
+app.use('/api/tickets', tickets_routes);
+app.use('/api/clients', clients_routes);
+app.use('/api/developers', developers_routes);
+app.use('/api/projects', projects_routes);
+app.use('/api/comments', comments_routes);
+app.use('/api/accesses', accesses_routes);
+app.use('/api/tags', tags_routes);
+app.use('/api/ticketTags', ticketTags_routes);
 
 app.listen(port, () => {
   console.log(`Server is running and listening on port ${port}`);
