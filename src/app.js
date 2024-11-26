5df814188670b31a733d0c3ec447bd19ed0a59e9
Rayan Capaldi <rayan.capaldi@imogate.eu>
2024-11-26 10:10:08 +0100

    Migration New RambHub

--- src/app.js
+++ src/app.js
@@ -7,10 +7,6 @@ const app = express();
 app.use(cors());
 
 const tickets_routes = require('./routes/routesTicket.js');
-const clients_routes = require('./routes/routesClient.js');
-const developers_routes = require('./routes/routesDeveloper.js');
-const projects_routes = require('./routes/routesProject.js');
-const comments_routes = require('./routes/routesComment.js');
 const accesses_routes = require('./routes/routesAccess.js');
 const tags_routes = require('./routes/routesTag.js');
 const ticketTags_routes = require('./routes/routesTicketTag.js');
@@ -25,12 +21,8 @@ mongoose.connect(uri)
 
 app.use(express.json());
 app.use('/api/tickets', tickets_routes);
-app.use('/api/clients', clients_routes);
-app.use('/api/developers', developers_routes);
 app.use('/api/projects', projects_routes);
 app.use('/api/comments', comments_routes);
-app.use('/api/accesses', accesses_routes);
-app.use('/api/tags', tags_routes);
 app.use('/api/ticketTags', ticketTags_routes);
 app.use('/api/subtasks', subtasks_routes);
 
