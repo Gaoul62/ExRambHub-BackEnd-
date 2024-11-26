77d47f750a95d8e8b32bffa8754a9ec36a14b849
Rayan Capaldi <rayan.capaldi@imogate.eu>
2024-11-26 13:42:13 +0100

    Migration 2

--- src/app.js
+++ src/app.js
@@ -6,11 +6,13 @@ const app = express();
 
 app.use(cors());
 
-const tickets_routes = require('./routes/routesTicket.js');
 const accesses_routes = require('./routes/routesAccess.js');
-const tags_routes = require('./routes/routesTag.js');
-const ticketTags_routes = require('./routes/routesTicketTag.js');
+const comments_routes = require('./routes/routesComment.js');
+const projects_routes = require('./routes/routesProject.js');
 const subtasks_routes = require('./routes/routesSubtask.js');
+const tag_routes = require('./routes/routesTag.js');
+const tickets_routes = require('./routes/routesTicket.js');
+const ticketTags_routes = require('./routes/routesTicketTag.js');
 
 const port = process.env.PORT || 3000;
 const uri = process.env.MONGODB_URI;
@@ -20,11 +22,13 @@ mongoose.connect(uri)
   .catch(error => console.error('Error:', error));
 
 app.use(express.json());
-app.use('/api/tickets', tickets_routes);
-app.use('/api/projects', projects_routes);
+app.use('/api/accesses', accesses_routes);
 app.use('/api/comments', comments_routes);
-app.use('/api/ticketTags', ticketTags_routes);
+app.use('/api/projects', projects_routes);
 app.use('/api/subtasks', subtasks_routes);
+app.use('/api/tags', tag_routes);
+app.use('/api/tickets', tickets_routes);
+app.use('/api/ticketTags', ticketTags_routes);
 
 app.listen(port, () => {
   console.log(`Server is running and listening on port ${port}`);
